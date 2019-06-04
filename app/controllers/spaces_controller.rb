be1f2744dd279c0c820f5abeedea4140fe439123
高村美紀 <takamuraminori@koumuramikinoMacBook-Pro.local>
2019-06-04 22:43:27 +0900

    space new

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,3 +1,18 @@
 class SpacesController < ApplicationController
 	before_action :authenticate_user!
+	def new
+	  @space = Space.new
+	  @spaces = current_user.spaces
+	end
+
+	def create
+  	  @space = current_user.spaces.new space_params
+  	  @space.save!
+  	  redirect_to new_space_path
+	end
+
+	private
+	def space_params
+      params.require(:space).permit(:name, :room, :r_at)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,15 @@ Rails.application.routes.draw do
   resources :spaces
   get "home/index" =>"home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    	get '/users/sign_out' => 'devise/sessions#destroy'
+  	end
+    authenticated :users do
+		root :to => "spaces#new"
+	end
+
+	unauthenticated :users do
+	    root :to => "spaces#new"
+	end
 end
